cc8eec84a0669e17ec0e99ac639e380b1c1c0a11
Sk <techjk021@gmail.com>
2024-07-09 00:50:10 +0530

    Fixed Previous orders issue

--- users/models.py
+++ users/models.py
@@ -29,6 +29,7 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    toShow = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.quantity} of {self.menu_item.name}"
